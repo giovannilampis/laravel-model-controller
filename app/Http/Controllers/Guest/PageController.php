c123efd146b71300acc912dfd03376d50b5c4b21
giovannilampis <giolampis@hotmail.com>
2023-06-11 12:19:20 +0200

    trying to debug error

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Models\Movie;
 
 class PageController extends Controller
 {
+    //metodo convenzionalmente chiamato 'index', recupera tutte le info dal db
     public function index()
     {
         $movies = Movie::all();
 
-        dd($movies);
+        // dd($movies);
 
-        return view('movies');
+        return view('home', compact('movies'));
     }
 }
